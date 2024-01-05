22ee777dae83ea74a479ccfa365a34bbac0d77a2
Vishal21121 <vs84077000@gmail.com>
2024-01-05 16:56:30 +0530

    moved files and folder and added one route for getting random questions

---
+++ backend/db/dbConfig.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var collection *mongo.Collection
+
+func Init() *mongo.Client {
+	// clientOption
+	connectionString := os.Getenv("MONGO_URI")
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	// connect to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// checking the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client
+}
---
+++ backend/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Vishal21121/CodeConcepts-vscode-extension/db"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func main() {
+	app := fiber.New()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	var client *mongo.Client = db.Init()
+	questionController := client.Database("extensionDatabase").Collection("questions")
+
+	app.Get("/questions", func(c *fiber.Ctx) error {
+
+		// getting all the query parameters
+		queries := c.Queries()
+		language := queries["language"]
+
+		if language == "" {
+			c.SendStatus(400)
+			return c.JSON(fiber.Map{
+				"statusCode": 400,
+				"data": fiber.Map{
+					"status":  "failure",
+					"message": "language is required",
+				},
+			})
+		}
+
+		// filter with language
+		filterWithLanguage := bson.D{
+			{"$match", bson.D{{"language", language}}},
+		}
+		// get the random element
+		randomElement := bson.D{{"$sample", bson.D{{"size", 1}}}}
+		cursor, err := questionController.Aggregate(c.Context(), mongo.Pipeline{filterWithLanguage, randomElement})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		var results []bson.M
+		if err = cursor.All(c.Context(), &results); err != nil {
+			log.Fatal(err)
+		}
+		c.SendStatus(200)
+		return c.JSON(fiber.Map{
+			"statusCode": 200,
+			"data": fiber.Map{
+				"status": "success",
+				"value":  results,
+			},
+		})
+	})
+	fmt.Println("Server is listening at port 3000")
+	log.Fatal(app.Listen(":3000"))
+}
