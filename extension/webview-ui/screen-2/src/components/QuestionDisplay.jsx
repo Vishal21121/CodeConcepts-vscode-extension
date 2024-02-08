9d079c24aeca12c42c0e24b3f6a38cd11ee08229
Vishal21121 <vs84077000@gmail.com>
2024-02-08 08:09:57 +0530

    added: delete button, update button and delete button handler

--- extension/webview-ui/screen-2/src/components/QuestionDisplay.jsx
+++ extension/webview-ui/screen-2/src/components/QuestionDisplay.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Renderer from './Renderer.jsx'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
 
 const QuestionDisplay = ({ vscode }) => {
     const [data, setData] = useState([])
@@ -20,13 +24,29 @@ const QuestionDisplay = ({ vscode }) => {
         })
     }, [])
 
+    const deleteButtonHandler = (id) => {
+        // console.log("id: ", id)
+        vscode.postMessage({
+            command: "deleteQuestion",
+            message: id
+        })
+    }
+
     return (
         <div className='w-full'>
             <div className='flex flex-col gap-2'>
                 {
                     data.length != 0 && data.map(({ question, answer, id, language }) => {
                         return (
-                            <div key={id}>
+                            <div key={id} className='w-full flex flex-col items-end'>
+                                <div className='flex'>
+                                    <VSCodeButton appearance='icon'>
+                                        <FaRegEdit />
+                                    </VSCodeButton>
+                                    <VSCodeButton appearance='icon' onClick={() => deleteButtonHandler(id)}>
+                                        <MdDelete />
+                                    </VSCodeButton>
+                                </div>
                                 <Renderer content={`${question}\n${answer}`} language={language} />
                             </div>
                         )
--- extension/webview-ui/screen-2/src/components/Renderer.jsx
+++ extension/webview-ui/screen-2/src/components/Renderer.jsx
@@ -4,16 +4,18 @@ import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const Renderer = ({ content, language }) => {
     return (
-        <SyntaxHighlighter
-            children={String(content).replace(/\n$/, '')}
-            style={dracula}
-            language={language.toLowerCase()}
-            customStyle={{
-                padding: "25px",
-                margin: "0px"
-            }}
-            wrapLongLines="true"
-        />
+        <div className='w-full'>
+            <SyntaxHighlighter
+                children={String(content).replace(/\n$/, '')}
+                style={dracula}
+                language={language.toLowerCase()}
+                customStyle={{
+                    padding: "25px",
+                    margin: "0px"
+                }}
+                wrapLongLines="true"
+            />
+        </div>
     )
 }
 
